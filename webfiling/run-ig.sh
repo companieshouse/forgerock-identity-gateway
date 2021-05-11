0612759ae51d56b55c6d25268a8a9441f205c00e
Saul France <saul.france@amido.com>
2021-05-11 12:01:38 +0100

    fix: replace application ip placeholder

--- webfiling/run-ig.sh
+++ webfiling/run-ig.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 sed "s|{APPLICATION_HOST}|$APPLICATION_HOST|g" /var/ig/config/routes/webfiling.json.tpl > /var/ig/config/routes/webfiling.json
+sed "s|{APPLICATION_IP}|$APPLICATION_IP|g" /var/ig/config/routes/webfiling.json > /var/ig/config/routes/webfiling.json
 
 rm /var/ig/config/routes/webfiling.json.tpl
 
