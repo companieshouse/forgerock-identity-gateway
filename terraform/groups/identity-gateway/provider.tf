b0ae5774df82212676c3e74d42e101af2f5037f0
Saul France <saul.france@amido.com>
2021-05-14 11:08:46 +0100

    fix: use environment for networking tf state

--- terraform/groups/identity-gateway/provider.tf
+++ terraform/groups/identity-gateway/provider.tf
@@ -12,7 +12,7 @@ terraform {
 data "terraform_remote_state" "networking" {
   backend = "s3"
   config = {
-    bucket = "development-${var.region}.terraform-state.ch.gov.uk"
+    bucket = "${var.environment}-${var.region}.terraform-state.ch.gov.uk"
     key    = "aws-common-infrastructure-terraform/common-${var.region}/networking.tfstate"
     region = var.region
   }
