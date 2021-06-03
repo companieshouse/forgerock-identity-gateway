a4708d084ec7e8df8b0d3207a62f11e587d3ef0c
Saul France <saul.france@amido.com>
2021-06-03 11:06:33 +0100

    chore: remove all .tpl files

--- webfiling/run-ig.sh
+++ webfiling/run-ig.sh
@@ -3,6 +3,6 @@
 sed -e "s|{APPLICATION_HOST}|$APPLICATION_HOST|g" -e "s|{APPLICATION_IP}|$APPLICATION_IP|g" /var/ig/config/routes/01-webfiling-default.json.tpl > /var/ig/config/routes/01-webfiling-default.json
 sed -e "s|{APPLICATION_HOST}|$APPLICATION_HOST|g" -e "s|{APPLICATION_IP}|$APPLICATION_IP|g" /var/ig/config/routes/10-webfiling-seclogin.json.tpl > /var/ig/config/routes/10-webfiling-seclogin.json
 
-rm /var/ig/config/routes/webfiling.json.tpl
+rm /var/ig/config/routes/*.tpl
 
 /opt/ig/bin/start.sh /var/ig
