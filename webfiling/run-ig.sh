80bbfddcd856beb3a847dad32fccdc35dd08bbbb
Saul France <saul.france@amido.com>
2021-06-09 11:13:22 +0100

    fix: remove default and runpage routes

--- webfiling/run-ig.sh
+++ webfiling/run-ig.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-sed -e "s|{APPLICATION_HOST}|$APPLICATION_HOST|g" -e "s|{APPLICATION_IP}|$APPLICATION_IP|g" /var/ig/config/routes/01-webfiling-default.json.tpl > /var/ig/config/routes/01-webfiling-default.json
 sed -e "s|{APPLICATION_HOST}|$APPLICATION_HOST|g" -e "s|{APPLICATION_IP}|$APPLICATION_IP|g" /var/ig/config/routes/10-webfiling-seclogin.json.tpl > /var/ig/config/routes/10-webfiling-seclogin.json
-sed -e "s|{APPLICATION_HOST}|$APPLICATION_HOST|g" -e "s|{APPLICATION_IP}|$APPLICATION_IP|g" /var/ig/config/routes/11-webfiling-runpage.json.tpl > /var/ig/config/routes/11-webfiling-runpage.json
 
 /opt/ig/bin/start.sh /var/ig
