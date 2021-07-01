c2967f86407989349307b2449c4fa390920a0b31
Saul France <saul.france@amido.com>
2021-07-01 15:35:28 +0100

    feat: check query strings for company number

--- webfiling/scripts/groovy/authRedirect.groovy
+++ webfiling/scripts/groovy/authRedirect.groovy
@@ -5,11 +5,21 @@ next.handle(context, request).thenOnResult(response -> {
    def locationUri
 
     if (response.getStatus().isRedirection() &&
-        (locationHeaders = response.headers.get("Location")) != null  &&
+        (locationHeaders = response.headers.get("Location")) != null &&
         (locationUri = locationHeaders.firstValue.toString()) ==~ "^https://" + routeArgFidcFqdn + "/am/oauth2/authorize.*") {
+            locationUri = locationHeaders.firstValue.toString()
+        def newUri =  routeArgAuthUri + "?goto=" + URLEncoder.encode(locationUri) + "&realm=/" + routeArgRealm + "&service=" + routeArgJourney + "&authIndexType=service&authIndexValue=" + routeArgJourney + "&mode=AUTHN_ONLY&ForceAuth=true"
+        // Line below for demo
+        // def newUri =  "https://idam.amido.aws.chdev.org/am/XUI?goto=" + URLEncoder.encode(locationUri) + "&realm=/" + routeArgRealm + "&service=" + routeArgJourney + "&authIndexType=service&authIndexValue=" + routeArgJourney + "&mode=AUTHN_ONLY&ForceAuth=true"
 
-       def newUri = routeArgAuthUri + "?goto=" + URLEncoder.encode(locationUri) + "&realm=/" + routeArgRealm + "&service=" + routeArgJourney + "&authIndexType=service&authIndexValue=" + routeArgJourney + "&mode=AUTHN_ONLY&ForceAuth=true"
-       response.headers.remove("Location")
-       response.headers.add("Location",newUri)
+        def queryParams = request.uri.query?.split('&')
+        if (queryParams != null) {
+            def mapParams = queryParams.collectEntries { param -> param.split('=').collect { URLDecoder.decode(it) }}
+            newUri += mapParams.companyNo ? "&companyNo=" + mapParams.companyNo : ""
+            newUri += mapParams.jurisdiction ? "&jurisdiction=" + mapParams.jurisdiction : ""
+        }
+
+        response.headers.remove("Location")
+        response.headers.add("Location",newUri)
    }
 })
