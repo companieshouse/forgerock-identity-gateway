d16539ed6d6205e12ecbf118803fe48f7f98fcfc
Saul France <saul.france@amido.com>
2021-08-12 13:15:12 +0100

    feat: add chs infrastructure

---
+++ terraform/groups/chs/modules/ecs-task/main.tf
@@ -0,0 +1,126 @@
+data "template_file" "container_definitions" {
+  template = file("${path.module}/templates/container_definitions.json.tpl")
+  vars = {
+    task_name                 = var.service_name
+    aws_ecr_url               = var.ecr_url
+    tag                       = var.container_image_version
+    cloudwatch_log_group_name = var.log_group_name
+    cloudwatch_log_prefix     = var.log_prefix
+    region                    = var.region
+    api_load_balancer         = var.api_load_balancer
+  }
+}
+
+resource "aws_ecs_task_definition" "ig" {
+  family                   = var.service_name
+  network_mode             = "awsvpc"
+  execution_role_arn       = var.ecs_task_role_arn
+  cpu                      = var.task_cpu
+  memory                   = var.task_memory
+  requires_compatibilities = ["FARGATE"]
+  container_definitions    = data.template_file.container_definitions.rendered
+
+  tags = var.tags
+}
+
+resource "aws_ecs_service" "ig" {
+  name            = var.service_name
+  cluster         = var.ecs_cluster_id
+  task_definition = aws_ecs_task_definition.ig.arn
+  desired_count   = var.autoscaling_min
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    security_groups  = [aws_security_group.instance.id]
+    subnets          = var.subnet_ids
+    assign_public_ip = false
+  }
+
+  # load_balancer {
+  #   target_group_arn = var.target_group_arn
+  #   container_name   = var.service_name
+  #   container_port   = 8080
+  # }
+
+  dynamic "load_balancer" {
+    for_each = var.target_group_arns
+    content {
+      target_group_arn = load_balancer.value
+      container_name   = var.service_name
+      container_port   = 8080
+    }
+  }
+
+  # Optional: Allow external changes without Terraform plan difference
+  lifecycle {
+    ignore_changes = [desired_count]
+  }
+
+  tags = var.tags
+}
+
+
+resource "aws_security_group" "instance" {
+  description = "Restricts access to ${var.service_name} instance"
+  name        = "${var.service_name}-instance"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port       = 8080
+    to_port         = 8080
+    protocol        = "tcp"
+    security_groups = var.lb_security_group_ids
+  }
+
+  egress {
+    description = "Allow outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = merge(var.tags, {
+    Name = "${var.service_name}-instance"
+  })
+}
+
+resource "aws_appautoscaling_target" "ig" {
+  max_capacity       = var.autoscaling_max
+  min_capacity       = var.autoscaling_min
+  resource_id        = "service/${var.ecs_cluster_name}/${aws_ecs_service.ig.name}"
+  scalable_dimension = "ecs:service:DesiredCount"
+  service_namespace  = "ecs"
+}
+
+resource "aws_appautoscaling_policy" "ig-cpu" {
+  name               = "${var.service_name} CPU Target"
+  policy_type        = "TargetTrackingScaling"
+  resource_id        = aws_appautoscaling_target.ig.resource_id
+  scalable_dimension = aws_appautoscaling_target.ig.scalable_dimension
+  service_namespace  = aws_appautoscaling_target.ig.service_namespace
+
+  target_tracking_scaling_policy_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ECSServiceAverageCPUUtilization"
+    }
+
+    target_value = var.target_cpu
+  }
+}
+
+resource "aws_appautoscaling_policy" "ig-memory" {
+  name               = "${var.service_name} Memory Target"
+  policy_type        = "TargetTrackingScaling"
+  resource_id        = aws_appautoscaling_target.ig.resource_id
+  scalable_dimension = aws_appautoscaling_target.ig.scalable_dimension
+  service_namespace  = aws_appautoscaling_target.ig.service_namespace
+
+  target_tracking_scaling_policy_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ECSServiceAverageMemoryUtilization"
+    }
+
+    target_value = var.target_memory
+  }
+}
---
+++ terraform/groups/chs/modules/ecs-task/variables.tf
@@ -0,0 +1,88 @@
+variable "region" {
+  type = string
+}
+
+variable "service_name" {
+  type = string
+}
+
+variable "vpc_id" {
+  type = string
+}
+
+variable "subnet_ids" {
+  type = list(string)
+}
+
+variable "ecs_cluster_id" {
+  type = string
+}
+
+variable "ecs_cluster_name" {
+  type = string
+}
+
+variable "ecs_task_role_arn" {
+  type = string
+}
+
+variable "lb_security_group_ids" {
+  type = list(string)
+}
+
+variable "container_image_version" {
+  type = string
+}
+
+variable "ecr_url" {
+  type = string
+}
+
+variable "task_cpu" {
+  type = number
+}
+
+variable "task_memory" {
+  type = number
+}
+
+variable "log_group_name" {
+  type = string
+}
+
+variable "log_prefix" {
+  type = string
+}
+
+variable "target_group_arns" {
+  type = list(string)
+}
+
+variable "tags" {
+  type = object({
+    Environment    = string
+    Service        = string
+    ServiceSubType = string
+    Team           = string
+  })
+}
+
+variable "autoscaling_min" {
+  type = number
+}
+
+variable "autoscaling_max" {
+  type = number
+}
+
+variable "target_cpu" {
+  type = number
+}
+
+variable "target_memory" {
+  type = number
+}
+
+variable "api_load_balancer" {
+  type = string
+}
