6ccb6daa0de7d46cece3241a06a178bb26bb8af8
Steve Davis <sdavis@companieshouse.gov.uk>
2021-11-24 08:45:27 +0000

    Pass CompanyNo to error urls

--- webfiling/scripts/groovy/errorRedirect.groovy
+++ webfiling/scripts/groovy/errorRedirect.groovy
@@ -4,7 +4,22 @@ next.handle(context, request).thenOnResult(response -> {
     println "[CHLOG][ERRORREDIRECT] password replay"
     println "[CHLOG][ERRORREDIRECT] path: " + request.uri.path
 
-    def location = routeArgAuthUri + routeArgErrorPath + '?context=' + routeArgContext
+    def companyNo = ""
+
+    if (attributes != null && attributes.openid != null && attributes.openid.id_token_claims != null) {
+        println "[CHLOG][ERRORREDIRECT] OpenID EWF claim : " + attributes.openid.id_token_claims['webfiling_info']
+
+        if (attributes.openid.id_token_claims['webfiling_info'] != null) {
+            def tokenCompanyNo = attributes.openid.id_token_claims['webfiling_info']['company_no']
+            if (companyNo != null) {
+                companyNo = tokenCompanyNo
+                println("[CHLOG][ERRORREDIRECT] CompanyNo = " + companyNo)
+            }
+        }
+    }
+
+    def location = routeArgAuthUri + routeArgErrorPath + '?context=' + routeArgContext + "&companyNo=" + companyNo
+
     response.setStatus(Status.FOUND)
     response.headers.add("Location", location)
 
