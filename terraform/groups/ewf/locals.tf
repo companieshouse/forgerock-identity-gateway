ab64654369272035393a97dd2cfd505b7b7705dc
pshdev0 <pshdev0@gmail.com>
2025-07-31 12:23:56 +0100

    undo added space

--- terraform/groups/ewf/locals.tf
+++ terraform/groups/ewf/locals.tf
@@ -5,7 +5,6 @@ locals {
     ServiceSubType = var.service_name
     Team           = "amido"
   }
-
   test_cidr_blocks  = var.test_access_enable ? jsondecode(data.vault_generic_secret.test_cidrs.data["cidrs"]) : []
 
   ingress_cidr_blocks = var.internal_access_only ? concat(values(data.vault_generic_secret.internal_cidrs.data), var.internal_access_cidrs, local.test_cidr_blocks) : concat(jsondecode(data.vault_generic_secret.external_cidrs.data["external_cidrs"]), var.internal_access_cidrs)
